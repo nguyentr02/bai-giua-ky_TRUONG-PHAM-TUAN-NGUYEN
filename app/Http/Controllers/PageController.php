1024158d4db572969f6abd718b77a290bd3ea8f4
nguyentr02 <nguyen.truong200226@vnuk.edu.vn>
2022-11-05 23:03:39 +0700

    FM

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Slide;
+use App\Models\Product;
+use App\Models\ProductType;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function getIndex()
+    {
+        $slide = Slide::all();
+        // return view('page.trangchu');
+        // return view('page.trangchu', compact('slide'));
+        $new_product = Product::where('new', 1)->paginate(8);
+        $sanpham_khuyenmai = Product::where('promotion_price', '<>', 0)->paginate(4);
+        return view('page.trangchu', compact('slide', 'new_product', 'sanpham_khuyenmai'));
+    }
+
+    public function getLoaiSp($type)
+    {
+        $type_product = ProductType::all();
+
+        $sp_theoloai = Product::where('id_type', $type)->get();
+
+        $sp_khac = Product::where('id_type', '<>', $type)->paginate(3);
+
+        $loai_sp = ProductType::where('id', $type)->first();
+
+        return view('page.loai_sanpham', compact('type_product', 'sp_theoloai', 'sp_khac', 'loai_sp'));
+    }
+
+    public function getModel()
+    {
+        return view('page.product_model');
+    }
+
+    public function getDetail()
+    {
+        return view('page.product_detail');
+    }
+
+    public function getContact()
+    {
+        return view('page.contact');
+    }
+
+    public function getAbout()
+    {
+        return view('page.about');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+
+Route::get('/trangchu', [PageController::class, 'getIndex']);
+Route::get('/product_model', [PageController::class, 'getModel']);
+Route::get('/product_detail', [PageController::class, 'getDetail']);
+
+Route::get('/type/{id}',[PageController::class, 'getLoaiSp']);
+
+Route::get('/detail/{id}',[PageController::class, 'getDetail']);
+
+Route::get('/contact', [PageController::class, 'getContact']);
+Route::get('/about', [PageController::class, 'getAbout']);
+
+Route::get('loai-san-pham/{type}', [PageController::class,'getLoaiSp']);
+
+Route::get('chi-tiet-san-pham', [PageController::class,'getLienhe']);
+
